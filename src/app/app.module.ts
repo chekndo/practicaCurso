fb0de917f6db9a73347c6d31bd6a50bcd7d68532
marcos <mckndo>
2019-09-25 13:20:09 +0200

    commitNGMODEL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaCorreoComponent } from './Components/lista-correo/lista-correo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ListaCorreoComponent } from './Components/lista-correo/lista-correo.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
